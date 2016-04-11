1a922747507b7b44f336f87f4f372224d57f941c
bliecl <luc.liechti@bbcag.ch>
2016-04-11 15:57:03 +0200

    Register Page started

--- view/adminFooter.php
+++ view/adminFooter.php
@@ -1,2 +1,4 @@
+  <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+
   </body>
 </html>
---
+++ view/register.php
@@ -0,0 +1,29 @@
+<div class="mdl-grid">
+  <div class="mdl-cell mdl-cell--2 mdl-cell--0-phone mdl-cell--0-tablet"></div>
+  <div class="mdl-cell mdl-cell--8 mdl-cell--12-phone mdl-cell--12-tablet">
+    <div class="container">
+      <form method="post">
+        <div class="mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="text" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">Vorname</label>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="text" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">Name</label>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="text" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">Username</label>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="password" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">Passwort</label>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="password" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">wiederholen</label>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
