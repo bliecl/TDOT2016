02e97e7037b8bc2cdf56c4352cc4229bc2e6fcd7
bwaltm <walter.michael98@gmail.com>
2016-04-11 13:51:14 +0200

    first transformed version
    removed not needed bootstrap content
    added mdl links

--- view/footer.php
+++ view/footer.php
@@ -1,11 +1,3 @@
-        <hr>
-        <footer>
-          <p>&copy; ICT Berufsbildungcenter AG 2014</p>
-        </footer>
-      </div>
 
-      <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-      <script src="/view/js/bootstrap.min.js"></script>
-    </div>
   </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -10,40 +10,10 @@
 
     <title><?php echo $title;?></title>
 
-    <link href="/view/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/view/css/custom.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" href="https://github.com/necolas/normalize.css/blob/master/normalize.css" type="text/css" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-blue.min.css" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400,300" type="text/css" />
   </head>
 
   <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">BBC-MVC</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="divider">&nbsp;</li>
-            <li><a href="/">Home</a></li>
-            <li><a href="/user">Benutzer</a></li>
-            <li><a href="/user/create">Benutzer erstellen</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="container theme-showcase" role="main">
-
-      <div class="container">
-
-        <h1><?php echo $heading;?></h1>
