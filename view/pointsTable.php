d990f62b30db1d1238c0499f150cd9923dccd8af
bwaltm <walter.michael98@gmail.com>
2016-04-12 10:37:48 +0200

    created Layout
    -Layout for addPoints
    -Layout for pointsTable
    -changed css Link in header
    -removed fail adminFooter

--- view/pointsTable.php
+++ view/pointsTable.php
@@ -1,1 +1,43 @@
-pointsTable
+<div class="mdl-grid">
+  <div class="mdl-cell mdl-cell--1 mdl-cell--0-phone mdl-cell--0-tablet"></div>
+  <div class="mdl-cell mdl-cell--8 mdl-cell--12-phone mdl-cell--12-tablet">
+    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp" id="pointsTable">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th class="mdl-data-table__cell--non-numeric">Username</th>
+          <th class="mdl-data-table__cell--non-numeric">Seite</th>
+          <th>Punkte</th>
+          <th>Zeit</th>
+          <th>Entfernen</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td class="mdl-data-table__cell--non-numeric">Testuser</td>
+          <td class="mdl-data-table__cell--non-numeric">Dark</td>
+          <td>50</td>
+          <td>12-30-10-23-13</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td class="mdl-data-table__cell--non-numeric">Ordis</td>
+          <td class="mdl-data-table__cell--non-numeric">Bright</td>
+          <td>120</td>
+          <td>12-30-10-23-13</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td class="mdl-data-table__cell--non-numeric">gibbix</td>
+          <td class="mdl-data-table__cell--non-numeric">Dark</td>
+          <td>23</td>
+          <td>12-30-10-23-13</td>
+          <td>X</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
