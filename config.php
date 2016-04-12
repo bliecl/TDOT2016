e37d133e3acb500962d0f004234610f3272c0f56
bwaltm <walter.michael98@gmail.com>
2016-04-12 10:12:40 +0200

    changed config file
    remove /phpmyadmin

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return array(
 
     'database' => array(
-        'host'     => 'bwaltm.dev.bbc-projects.ch/phpmyadmin',
+        'host'     => 'bwaltm.dev.bbc-projects.ch',
         'username' => 'bwaltmsql1',
         'password' => 'xrauVkiPmd',
         'database' => 'bwaltmsql1',
