acd03ed01970579c70a59f9c21a931ad10456499
bwaltm <walter.michael98@gmail.com>
2016-04-12 08:24:38 +0200

    included main page in project

--- view/default_index.php
+++ view/default_index.php
@@ -1,32 +1,37 @@
 
 <div id="container">
-	<p class="grey center big">
+	<h1 class="grey center big">
 		The Star Wars Battle
-	</p>
+	</h1>
+	<br/>
 	<div id="bright_side">
-		<div id="header">
-			<p class="blue">
+		<img id="bright_image" src="http://i.dailymail.co.uk/i/pix/2015/12/03/22/2EDC2B6C00000578-3345081-Taking_up_the_fight_Mark_Hamill_wields_his_lightsaber_as_heroic_-a-1_1449181084699.jpg" alt="" title="">
+		<div id="bright_header">
+			<p class="blue middle">
 				Bright Side
 			</p>
 		</div>
 		<div id="bright_side_statistic">
+
 		</div>
 		<div>
-			<p class="blue big">
+			<p id="bright_counter" class="blue middle">
 				200 Punkte
 			</p>
 		</div>
 	</div>
 	<div id="dark_side">
-		<div id="header">
-			<p class="red">
+		<img id="dark_image" src="http://vignette2.wikia.nocookie.net/starwars/images/7/7d/Star_Wars_Darth_Vader_Vol_1_1_GameStop_Variant.jpg/revision/latest?cb=20150122141514" alt="" title="">
+		<div id="dark_header">
+			<p class="red middle">
 				Dark Side
 			</p>
 		</div>
 		<div id="dark_side_statistic">
-			</div>
+
+		</div>
 		<div>
-			<p class="red big">
+			<p id="dark_counter" class="red middle">
 				200 Punkte
 			</p>
 		</div>
