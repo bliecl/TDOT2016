6d92a9a5bfb95b4d4b3f5d30228f86455b41fab5
bliecl <luc.liechti@bbcag.ch>
2016-04-14 09:13:12 +0200

    Pointpage Responsive with lightsabers

--- view/default_index.php
+++ view/default_index.php
@@ -1,78 +1,104 @@
-<div id="container">
-	<h1 class="grey center big">
-		The Space Battle
-	</h1>
-	<br/>
-	<div id="bright_side">
-		<div id="bright_header">
-			<h2 class="middle">
-				Light Side
-			</h2>
-		</div>
-		<div id="bright_side_statistic">
-			<div id="placeholderBlueHandle">
-				<div id="placeholderBlueBlade">
-					<div id="placeholderBlueTip"></div>
-					<div>
-						<img id="blueTip" src="/view/img/lightsaber/blue/tip.png" alt="blue tip">
-					</div>
-				</div>
-				<div>
-					<img id="blueBlade" src="/view/img/lightsaber/blue/blade.png" alt="blue blade">
-				</div>
-			</div>
-			<div>
-				<img id="blueHandle" src="/view/img/lightsaber/blue/handle.png" alt="blue handle">
-			</div>
-		</div>
-		<div>
-			<h2 id="bright_counter" class="middle">
-				200 Punkte
-			</h2>
+<div class="container">
+	<div class="mdl-grid">
+		<div class="mdl-cell mdl-cell--12-col">
+			<h1>
+				The Space Battle
+			</h1>
 		</div>
 	</div>
-	<div id="dark_side">
-		<div id="dark_header">
-			<h2 class="middle">
-				Dark Side
-			</h2>
-		</div>
-		<div id="dark_side_statistic">
-			<div id="placeholderRedHandle">
-				<div id="placeholderRedBlade">
-					<div id="placeholderRedTip"></div>
-					<div>
-						<img id="redTip" src="/view/img/lightsaber/red/tip.png" alt="red tip">
+	<div class="mdl-grid">
+		<div class="mdl-cell mdl-cell--2-offset-desktop mdl-cell--4-col-desktop
+		mdl-cell--8-col-tablet mdl-cell--8-col-phone">
+		<div class="lightSide">
+			<div class="mdl-grid">
+				<h2>Light Side</h2>
+			</div>
+			<div class="mdl-grid light_sword">
+				<div class="blueSword">
+					<div class="placeholderBlue"></div>
+					<!-- Lichtschwert Blau -->
+					<div id="lichtschwertBlau">
+						<div class="example-item clearfix">
+							<div class="lightsaberBlue">
+								<label for="obi-wan-example"></label>
+								<input type="checkbox" id="obi-wan-example">
+								<div class="switch"></div>
+								<div class="plasmaBlue obi-wan"></div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div>
-					<img id="redBlade" src="/view/img/lightsaber/red/blade.png" alt="red blade">
-				</div>
 			</div>
-			<div>
-				<img id="redHandle" src="/view/img/lightsaber/red/handle.png" alt="red handle">
+			<div class="mdl-grid">
+				<h2 id="bright_counter">
+					200 Punkte
+				</h2>
 			</div>
 		</div>
-		<div>
-			<h2 id="dark_counter" class="middle">
+	</div>
+	<div class="mdl-cell mdl-cell--4-col-desktop
+	mdl-cell--8-col-tablet mdl-cell--8-col-phone">
+	<div class="darkSide">
+		<div class="mdl-grid ">
+			<h2>Dark Side</h2>
+		</div>
+		<div class="mdl-grid dark_sword">
+			<div class="redSword">
+				<div class="placeholderRed"></div>
+				<!-- Lichtschwert Rot -->
+				<div id="lichtschwertRot">
+					<div class="example-item">
+						<div class="lightsaberRed">
+							<label for="darth-vader-example"></label>
+							<input type="checkbox" id="darth-vader-example">
+							<div class="switch"></div>
+							<div class="plasmaRed vader"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mdl-grid">
+			<h2 id="dark_counter">
 				200 Punkte
 			</h2>
 		</div>
 	</div>
 </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <script>
-	$(document).ready(function(){
-		setInterval(function(){
-			$.getJSON("/stats/getCurrentStats").success(function (data) {
-				var bright = data["bright"];
-				var dark = data["dark"];
-				$("#bright_counter").html(bright + " Punkte");
-				$("#dark_counter").html(dark + " Punkte");
-			});
-		},1000);
-
-
-	});
-
+$(document).ready(function(){
+	setInterval(function(){
+		$.getJSON("/stats/getCurrentStats").success(function (data) {
+			var bright = data["bright"];
+			var dark = data["dark"];
+			$("#bright_counter").html(bright + " Punkte");
+			$("#dark_counter").html(dark + " Punkte");
+		});
+	},1000);
+});
 </script>
